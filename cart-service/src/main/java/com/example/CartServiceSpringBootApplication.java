18335319cc81eeb0a24486b87420f93eba3795d4
ILOveMyasko <brutalkin_v@mail.ru>
2025-04-20 22:27:01 +0300

    created module cart-service

---
+++ cart-service/src/main/java/com/example/CartServiceSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServiceSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartServiceSpringBootApplication.class, args);
+    }
+}
